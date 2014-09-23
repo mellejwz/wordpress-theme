b76f8c599d9cf99159ccf1264b188c0a87509d13
unknown <mellezwerver@outlook.com>
2014-09-23 18:29:55 +0200

    completed responsive layout

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-/*$(document).ready(function(){
-	$('.menu-item').mouseover(function(){
+$(document).ready(function(){
+	/*$('.menu-item').mouseover(function(){
 		$(this).animate({paddingTop: "+=10"}, 500);
 	});
 	$('.menu-item').mouseout(function(){
 		$(this).animate({paddingTop: "-=10"}, 500);
+	});*/
+	$('.mobile_menu-button_clicked').hide(0);
+
+	$('.mobile_menu-button').click(function(){
+		$('#container').animate({left: "+=200px"}, 300, 'easeOutCirc');
+		$('.mobile_menu-button').hide(0);
+		$('.mobile_menu-button_clicked').show(0);
 	});
-});*/
+
+	$('.mobile_menu-button_clicked').click(function(){
+		$('#container').animate({left: "-=200px"}, 300, 'easeOutCirc');
+		$('.mobile_menu-button_clicked').hide(0);
+		$('.mobile_menu-button').show(0);
+	});
+});
